d93977f954bb443225693d7d5bee41241cadee20
Prasanthi Gogineni <gprashanthi.dev@gmail.com>
2024-09-27 14:31:03 -0500

    modified

--- Playbooks/Shell/create-servers.sh
+++ Playbooks/Shell/create-servers.sh
@@ -16,7 +16,7 @@ do
   echo "Creating $i instance"
 
   # Corrected PrivateIpAddress retrieval and closing parentheses
-  IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+  IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PublicIpAddress')
 
   echo "Created $i instance: $IP_ADDRESS"
 
